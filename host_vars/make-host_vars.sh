2d5df08858aa80cb4ae04f768c60eba9fec55fc5
simuns <sih@synack.fo>
2022-03-02 16:29:19 +0100

    changed template

--- host_vars/make-host_vars.sh
+++ host_vars/make-host_vars.sh
@@ -2,7 +2,7 @@ hosts=$(grep -Fvx -f .inv_empty.ini $1 | grep -Ev "^#|^$")
 for line in $hosts
 do
     echo "Generated host_var file: $line.yml"
-    cp host.yml.example $line.yml
+    cp ../roles/rsync-backup/defaults/main.yml $line.yml
 done
 echo "------------------------------"
 echo "Now edit them to your likings"
