d61ab205eb30f86a10fc0516815adb7cbe3db4b5
Símun Højgaard Lutzen <43766767+Simuns@users.noreply.github.com>
2022-03-04 00:28:23 +0100

    deleted unnessasary lines

--- roles/rsync-backup/templates/backup.sh
+++ roles/rsync-backup/templates/backup.sh
@@ -8,8 +8,7 @@
 # 3. $SOURCE & $DESTINATION
 # 4. user@yourserver.se for the mysqldump 
 # 5. --password=SUPERSECRET
-###Crontab
-# 0 12 * * 2 service-backup-rsync /mnt/rustdisk/backup-rsync//simunz.com/backup.sh
+
 TODAY=`date +"%Y%m%d"`
 YESTERDAY=`date -d "1 day ago" +"%Y%m%d"`
 OLDBACKUP=`{{ OLDBACKUP }}`
